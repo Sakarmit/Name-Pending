113b037291d0cbacf1f9a558791b0c0e0ee7a6be
Evan <eyoussef128@gmail.com>
2022-09-09 18:04:38 -0400

    Changed jump to AddForce

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // set rb to the Rigidbody component on the circle
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float movementForce = (float)0.05;
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            transform.position += new Vector3(0, movementForce, 0);
+            rb.AddForce(new Vector2(0, 400));//* Time.deltaTime));
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3(-movementForce, 0, 0);
+            transform.position += new Vector3(-movementForce*300, 0, 0) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
