bf787b99d7a20eaedced518483420d57b99f5bbf
CaptainIndia <ayirakas28@gmail.com>
2022-09-11 13:13:39 -0400

    Movement Fixed
    - WASD and Arrow can now be used to control character
    - Jumping now checks if player is on ground (no more infinite jumping)
    - Movement now uses velocity instead of force for more consistent movement
    - Added Platform Layer
    - Removed Temporary NewbehaviorScript

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,37 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    [SerializeField] private LayerMask platformLayerMask;
 
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider2D;
+
+    private float horizontalMovement = 0;
+    private float verticalMovement = 0;
+    
+    float movementSpeed = 5f;
+    float jumpSpeed = 8f;
+
+    // Start is called before the first frame update.
     void Start()
     {
         // set rb to the Rigidbody component on the circle
         rb = gameObject.GetComponent<Rigidbody2D>();
+        boxCollider2D = gameObject.GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float movementForce = (float)0.05;
-        if (Input.GetKeyDown(KeyCode.W))
+        //Gets User Inputs A/Left and D/Right
+        horizontalMovement = Input.GetAxis("Horizontal");
+        //Gets User Inputs W/Up and S/Down
+        verticalMovement = Input.GetAxis("Vertical");
+
+    }
+    void FixedUpdate()
+    {
+        // Player Jumping
+        if (IsGrounded() && verticalMovement > 0)
         {
-            rb.AddForce(new Vector2(0, 400));//* Time.deltaTime));
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
         }
-        if (Input.GetKey(KeyCode.A))
+
+        //Player X-Axis Movement
+        if(horizontalMovement != 0)
         {
-            transform.position += new Vector3(-movementForce*300, 0, 0) * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
+            rb.velocity = new Vector2(horizontalMovement * movementSpeed, rb.velocity.y);
+        } 
+        else
         {
-            transform.position += new Vector3(0, -movementForce, 0);
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += new Vector3(movementForce, 0, 0);
-        }
+        
+    }
+
+    //Uses BoxCast to Detect if Platform Layer Object is just under the player
+    private bool IsGrounded()
+    {
+        float extraDistance = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, extraDistance, platformLayerMask);
+        return raycastHit.collider != null;
     }
 }
